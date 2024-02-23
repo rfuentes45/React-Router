3d58b896fa11df313a688e2c6d32e60938a5778c
ricardo fuentes <richard45oen@gmail.com>
2024-02-23 14:11:06 -0600

    finalcode

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-
+import {Routes, Route, Link} from 'react-router-dom' // this is what we imported 
+import Red from './components/Red' // this is what we imported 
+import Blue from './components/Blue' // this is what we imported 
+import Home from './components/Home' // this is what we imported 
 
 function App() {
   
-
-  return (
+  return (  // the empty brackets mean React Fragment
     <>
-   
+   <div id="container">
+      
+      <div id="navbar">  
+      <Link to = "/Blue"> Blue</Link>
+      <Link to = "/Red"> Red </Link>
+      <Link to = "/">  Home </Link>
+      </div> 
+     
+      
+      <div id="main-section">{/* routes here */}
+      
+      <Routes>  
+      <Route path="/blue" Component={Blue}/> 
+      <Route path="/red" Component={Red}/>
+      <Route path="/" Component={Home}/> // we use the 'component' keyword 
+      </Routes>
+      
+      </div>
+    
+    </div>
     </>
   )
 }
